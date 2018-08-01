7ae06f02f4cb3564c5e1ef1012ebc42acab1ccfa
Jason Barnett <jason.w.barnett@gmail.com>
2018-08-01 09:48:30 -0400

    deps

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	_ "github.com/denisenkom/go-mssqldb"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func main() {
+	fmt.Println("Jason")
+}
